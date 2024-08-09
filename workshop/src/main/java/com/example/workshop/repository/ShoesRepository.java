bbee96f684002a13174b5a7cf8e2950b08bb189c
Abhinav Dhakal <dhakalabhinav4@gmail.com>
2024-08-09 18:53:49 +0545

    Delete workshop directory

--- workshop/src/main/java/com/example/workshop/repository/ShoesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.workshop.repository;
-
-import com.example.workshop.entity.Shoes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoesRepository extends JpaRepository<Shoes, Integer> {
-}
